49cac4fe22f6563ad1074baa6d90dc6c497b5143
devlavanere <michel.lavanere@academico.ifpb.edu.br>
2024-02-02 10:33:05 -0300

    app construido

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import Topo from './componentes/Topo';
 import SecaoBanner from './componentes/SecaoBanner';
 import SecaoExperienciaTrabalho from './componentes/SecaoExperienciaTrabalho';
@@ -5,13 +6,19 @@ import Rodape from './componentes/Rodape';
 import './App.css';
 
 export default function App() {
+
+	const [ehTemaEscuro, setTemaEscuro] = useState(false)
+
+  const alterarTema = () => {
+    setTemaEscuro(!ehTemaEscuro)
+  }
+
   return (
     <main>
-      <Topo />
-      <SecaoBanner />
-      <SecaoExperienciaTrabalho />
-      <Rodape />
-    </main>
+	    <Topo alterarTema={alterarTema} ehTemaEscuro={ehTemaEscuro}/>
+      <SecaoBanner ehTemaEscuro={ehTemaEscuro}/>
+      <SecaoExperienciaTrabalho ehTemaEscuro={ehTemaEscuro}/>
+      <Rodape ehTemaEscuro={ehTemaEscuro}/>
+		</main>
   );
 }
-
